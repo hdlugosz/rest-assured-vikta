e9702fdd2b8ab1f857189aac8e75e66b018e6545
Hubert Długosz <hdlugosz99@gmail.com>
2021-10-05 16:23:32 +0200

    Add tests using RestAssured, MySQL, config and logging

---
+++ src/test/java/Entity/Address.java
@@ -0,0 +1,40 @@
+package Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import static test.Utils.generateID;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class Address {
+    private String addressNickname;
+    private String cityName;
+    private int id;
+    private String postalCode;
+    private String regionName;
+    private String street;
+    private String streetAdditional;
+    private int userId;
+
+    public static class AddressBuilder {
+        public AddressBuilder withTestValues() {
+            this.addressNickname("testAddressNickname")
+                    .cityName("testCityName")
+                    .id(8888)
+                    .postalCode("20200")
+                    .regionName("testRegionName")
+                    .street("testStreet")
+                    .streetAdditional("testStreetAdditional")
+                    .userId(555);
+            return this;
+        }
+
+        public AddressBuilder randomId() {
+            this.id(generateID());
+            return this;
+        }
+    }
+}
---
+++ src/test/java/test/Utils.java
@@ -0,0 +1,13 @@
+package test;
+
+public final class Utils {
+    private final static int MIN_ID = 1000;
+    private final static int MAX_ID = 9999;
+
+    private Utils() {
+    }
+
+    public static int generateID() {
+        return (int) ((Math.random() * (MAX_ID - MIN_ID)) + MIN_ID);
+    }
+}
